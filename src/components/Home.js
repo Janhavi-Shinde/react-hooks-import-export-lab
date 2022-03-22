3b239123b9e1ece0ddd48e27aafa654a0a6f4ffa
janhavi <janashinde394@gmail.com>
2022-03-22 22:55:09 +1100

    all tests passing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import * as userFunctions from "../data/user";
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {userFunctions.username} is a Web Developer from {userFunctions.city}
       </h1>
     </div>
   );
 }
+
+export default Home;
